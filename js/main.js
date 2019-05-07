328e663763d355f32675b2a352e09b2b7d994f0d
JJ Yang <rongyang610@gmail.com>
2019-05-06 23:54:56 -0400

    Deleted some items

--- js/main.js
+++ js/main.js
@@ -230,43 +230,43 @@
 
 	};
 
-	var stickyFunction = function() {
+	// var stickyFunction = function() {
 
-		var h = $('.image-content').outerHeight();
+	// 	var h = $('.image-content').outerHeight();
 
-		if ($(window).width() <= 992 ) {
-			$("#sticky_item").trigger("sticky_kit:detach");
-		} else {
-			$('.sticky-parent').removeClass('stick-detach');
-			$("#sticky_item").trigger("sticky_kit:detach");
-			$("#sticky_item").trigger("sticky_kit:unstick");
-		}
+	// 	if ($(window).width() <= 992 ) {
+	// 		$("#sticky_item").trigger("sticky_kit:detach");
+	// 	} else {
+	// 		$('.sticky-parent').removeClass('stick-detach');
+	// 		$("#sticky_item").trigger("sticky_kit:detach");
+	// 		$("#sticky_item").trigger("sticky_kit:unstick");
+	// 	}
 
-		$(window).resize(function(){
-			var h = $('.image-content').outerHeight();
-			$('.sticky-parent').css('height', h);
+	// 	$(window).resize(function(){
+	// 		var h = $('.image-content').outerHeight();
+	// 		$('.sticky-parent').css('height', h);
 
 
-			if ($(window).width() <= 992 ) {
-				$("#sticky_item").trigger("sticky_kit:detach");
-			} else {
-				$('.sticky-parent').removeClass('stick-detach');
-				$("#sticky_item").trigger("sticky_kit:detach");
-				$("#sticky_item").trigger("sticky_kit:unstick");
+	// 		if ($(window).width() <= 992 ) {
+	// 			$("#sticky_item").trigger("sticky_kit:detach");
+	// 		} else {
+	// 			$('.sticky-parent').removeClass('stick-detach');
+	// 			$("#sticky_item").trigger("sticky_kit:detach");
+	// 			$("#sticky_item").trigger("sticky_kit:unstick");
 
-				$("#sticky_item").stick_in_parent();
-			}
+	// 			$("#sticky_item").stick_in_parent();
+	// 		}
 			
 
 			
 
-		});
+	// 	});
 
-		$('.sticky-parent').css('height', h);
+	// 	$('.sticky-parent').css('height', h);
 
-		$("#sticky_item").stick_in_parent();
+	// 	$("#sticky_item").stick_in_parent();
 
-	};
+	// };
 
 	var owlCrouselFeatureSlide = function() {
 		$('.owl-carousel').owlCarousel({
@@ -302,7 +302,7 @@
 
 		mobileMenuOutsideClick();
 		sliderMain();
-		stickyFunction();
+		// stickyFunction();
 		owlCrouselFeatureSlide();
 	});
 
